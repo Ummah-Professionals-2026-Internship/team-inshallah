f17b65b79caefd7a7091d3a111dc8bf3c26f8588
mizbahh <mizbahateeq@gmail.com>
2026-07-09 16:54:50 +0400

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import SignupPage from "./components/SignupPage";
 import EmailVerification from "./components/EmailVerification";
 import StudentForm from './StudentForm';
 import ProfessionalForm from './ProfessionalForm';
+import StudentDashboard from './components/StudentDashboard';
+import ProfessionalDashboard from './components/ProfessionalDashboard';
 
 export default function App() {
     return (
@@ -14,6 +16,12 @@ export default function App() {
                 <Route path="/verify-email" element={<EmailVerification />} />
                 <Route path="/student-form" element={<StudentForm />} />
                 <Route path="/professional-form" element={<ProfessionalForm />} />
+                <Route path="/verify-email" element={<EmailVerification />} />
+                <Route path="/student-form" element={<StudentForm />} />
+                <Route path="/professional-form" element={<ProfessionalForm />} />
+                <Route path="/student-dashboard" element={<StudentDashboard />} />
+                <Route path="/professional-dashboard" element={<ProfessionalDashboard />} />
+
             </Routes>
         </BrowserRouter>
     );
